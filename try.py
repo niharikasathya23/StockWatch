2d23e91166c51f6b4cb0ef783e5402d2b656694b
Nishee Shelat <you@example.com>
2023-03-04 09:47:12 -0700

    try updated

--- try.py
+++ try.py
@@ -1,1 +1,11 @@
 print("hello")
+def sentiment_analysis(tweet):
+    analysis = TextBlob(tweet)
+    sentiment = analysis.sentiment.polarity
+
+    if sentiment > 0:
+        return 'Positive'
+    elif sentiment == 0:
+        return 'Neutral'
+    else:
+        return 'Negative'
